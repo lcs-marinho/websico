48ef1d25110b042cfb13bcd5b29b484d3db08133
lucas.marinho <lucasneyson@gmail.com>
2024-06-02 22:54:29 -0300

    feat

--- src/page/home/home.component.ts
+++ src/page/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CarrouselComponent } from 'src/shared/components/carrousel/carrousel.component';
 import { ContentOneComponent } from 'src/shared/components/content-one/content-one.component';
+import { ContentThirdComponent } from 'src/shared/components/content-third/content-third.component';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [CommonModule,CarrouselComponent, ContentOneComponent],
+  imports: [CommonModule,CarrouselComponent, ContentOneComponent, ContentThirdComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
---
+++ src/shared/components/content-third/content-third.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-content-third',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './content-third.component.html',
+  styleUrls: ['./content-third.component.scss']
+})
+export class ContentThirdComponent {
+
+}
