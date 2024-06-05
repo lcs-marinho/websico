dafb03da729e54a086057e2d78bcb7186cbd8cc3
lucas.marinho <lucasneyson@gmail.com>
2024-06-04 22:49:02 -0300

    refact forms

--- src/page/questionnaire/questionnaire.component.ts
+++ src/page/questionnaire/questionnaire.component.ts
@@ -23,10 +23,24 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   ]
 })
 export class QuestionnaireComponent {
+
+  poll?: {
+    nome: string
+    faixaEtaria: string
+    sexo: string
+    estado: string
+    ouviuFalarDeAvaliacaoPsicologica: string
+    participouDeAvaliacaoPsicologica: string
+    experienciaParticipacao: string
+    motivacao: string
+    interesseFuturo: string
+    importanciaAvaliacaoPsicologicaComentario: ""
+  };
+
   currentQuestionIndex: number = 0;
   questions = [
     { question: 'Qual é o seu nome?', type: 'text', answer: '' },
-    { question: 'Qual é a sua idade?', type: 'number', answer: '' },
+    { question: 'Qual sua faixa etária?', type: 'select', options: ['3-18 anos','19-29  anos','30-39  anos','40-49  anos','50-59 anos','60-69 anos'] ,answer: '' },
     { question: 'Seu sexo', type: 'radio', options: ['Masculino', 'Feminino', 'Prefiro não responder'], answer: '' },
     { 
       question: 'Estado em que reside', 
@@ -36,15 +50,39 @@ export class QuestionnaireComponent {
       answer: ''
     },
     { 
-      question: 'Você sabe o que é uma avaliação psicológica?', 
+      question: 'Você já ouviu falar de avaliação psicológica?', 
       type: 'radio', 
-      options: ['Sim', 'Não', 'Não tenho certeza'], 
+      options: ['Sim', 'Não'], 
       answer: '' 
     },
     { 
-      question: 'Você sabe o que é uma avaliação psicológica?', 
+      question: 'Já participou de um processo de avaliação psicológica?', 
       type: 'radio', 
-      options: ['Sim', 'Não', 'Não tenho certeza'], 
+      options: ['Sim', 'Não'], 
+      answer: '' 
+    },
+    { 
+      question: 'Como foi sua experiência ao participar?', 
+      type: 'radio', 
+      options: ['Excelente', 'Boa','Regular','Ruim','Péssima','Não participei'], 
+      answer: '' 
+    },
+    { 
+      question: 'Qual foi sua motivação?', 
+      type: 'select', 
+      options: ['Encaminhamento médico', 'Interesse próprio','Contexto profissional','Contexto escolar','Psicotécnico','Não se aplica','Indicação de parentesco', 'Outros'], 
+      answer: '' 
+    },
+    // Caso nao saiba, o que é avaliação psicologica
+    { 
+      question: 'Teria interesse em aprender/particiar de uma futura avaliação?', 
+      type: 'radio', 
+      options: ['Sim','Não'], 
+      answer: '' 
+    },
+    { 
+      question: 'Acredita que avaliação psicológica seja importante? Deixe um comentário!', 
+      type: 'text', 
       answer: '' 
     },
     { 
@@ -53,12 +91,34 @@ export class QuestionnaireComponent {
   ];
 
   nextQuestion() {
+    
+    if(this.questions[4].answer == 'Não' && this.currentQuestionIndex == 4 ) {
+      this.currentQuestionIndex = 8;
+      return;
+    }
+
+    if(this.currentQuestionIndex == 7 ) {
+      this.currentQuestionIndex = 9;
+      return;
+    }
+
     if (this.currentQuestionIndex < this.questions.length - 1) {
       this.currentQuestionIndex++;
     }
   }
 
   previousQuestion() {
+
+    if(this.currentQuestionIndex == 8) {
+      this.currentQuestionIndex = 4;
+      return;
+    }
+
+    if(this.questions[4].answer == 'Sim' && this.currentQuestionIndex == 9) {
+      this.currentQuestionIndex = 7;
+      return;
+    }
+
     if (this.currentQuestionIndex > 0) {
       this.currentQuestionIndex = this.currentQuestionIndex - 1;
     }
