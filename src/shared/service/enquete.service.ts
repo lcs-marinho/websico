c74acc8a797fa89395420c3961698d7e56f629e5
lucas.marinho <lucasneyson@gmail.com>
2024-06-18 22:33:57 -0300

    comentary feat

--- src/shared/service/enquete.service.ts
+++ src/shared/service/enquete.service.ts
@@ -8,11 +8,17 @@ import { Observable } from 'rxjs';
 export class EnqueteService {
 
   private readonly api_localhost = 'http://localhost:8000/api';
-  private readonly api_server = 'https://websico.com.br/api'
+  private readonly api_server = 'https://websico.com.br/api';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   sendResponse(valueForm: any): Observable<any> {
     return this.http.post(`${this.api_server}/register_psichology`, valueForm);
   }
+
+  getComentarios(): Observable<any> {
+    return this.http.get(`${this.api_server}/get_descriptions`)
+  }
+
+  
 }
